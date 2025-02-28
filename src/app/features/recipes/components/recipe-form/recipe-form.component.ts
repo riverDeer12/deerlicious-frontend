96a71a8e85c18e26c269999ebe5c6655fa5f9ea6
riverDeer12 <mtrbojevic12@gmail.com>
2025-02-28 19:35:21 +0100

    bugfix for not closing dialog-form.component.html

--- src/app/features/recipes/components/recipe-form/recipe-form.component.ts
+++ src/app/features/recipes/components/recipe-form/recipe-form.component.ts
@@ -61,8 +61,6 @@ export class RecipeFormComponent {
     submit() {
         this.loadingData = true;
 
-        console.log(this.form.value);
-
         if (this.form.invalid) {
 
             this.form.markAllAsTouched();
